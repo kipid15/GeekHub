987ce9aff437e79bba411225d276b3511343d887
Kipid <syafiq.dosuki@gmail.com>
2018-03-09 01:25:04 +0800

    new page
    2nd page for data viewing

--- SimpleProject/SimpleProject/urls.py
+++ SimpleProject/SimpleProject/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf.urls import url
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include ('mainPage.urls')),
+    url(r'^thanks.$',views.thanks)
 ]
--- SimpleProject/mainPage/models.py
+++ SimpleProject/mainPage/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Data(models.Model):
+    inputName = models.CharField(max_length=200)
+    inputEmail = models.EmailField()
+    inputTel = models.IntegerField()
+    submitTime = models.DateTimeField()
