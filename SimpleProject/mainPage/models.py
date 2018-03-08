83c2cc3c59a03331e0252bb5f03c2bf866efca54
Kipid <syafiq.dosuki@gmail.com>
2018-03-09 03:31:53 +0800

    table for data output
    halfway thru.  cont. tomorrow

--- SimpleProject/mainPage/models.py
+++ SimpleProject/mainPage/models.py
@@ -6,4 +6,8 @@ class Data(models.Model):
     inputName = models.CharField(max_length=200)
     inputEmail = models.EmailField()
     inputTel = models.IntegerField()
-    submitTime = models.DateTimeField()
+    submitTime = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.inputName
--- SimpleProject/mainPage/views.py
+++ SimpleProject/mainPage/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Data
 
 # Create your views here.
 def index(request):
     return render(request, 'mainPage/home.html')
 
 def thanks(request):
-    return render(request, 'mainPage/thanks.html')
+    datas = Data.objects.all().order_by('submitTime')
+    return render(request, 'mainPage/thanks.html', {'datas': datas})
